d248ef3cdb4a9fe022f4fa239a78a0e5434d51f0
biel-huangchunqi <huangyaoyao2021@163.com>
2022-07-15 16:54:24 +0800

    INF-1148 package of configcat logging level

--- configcat_logging_level/settings.py
+++ configcat_logging_level/settings.py
@@ -2,8 +2,11 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    CANFIGCAT_POLL_INTERVAL_SECONDS: int = (60,)
     EXECUTION_ENV: str = "default"
-    CONFIGCAT_SDK: str = ""
+    CONFIGCAT_SDK: str = None
+    FEATURE_FLAG_NAME: str = "LOGGER_LEVEL"
+    MOCK_LOGGING_LEVEL: str = "DEFAULT"
 
     class Config:
         env_file = ".env"
